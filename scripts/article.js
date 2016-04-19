8c85db1f246cb9edf0510244fdd4c282f06e61e5
Hannah Gaskins <gaskinshannah@gmail.com>
2016-04-19 13:14:54 -0700

    added starter code to new branch day-07

--- scripts/article.js
+++ scripts/article.js
@@ -1,3 +1,5 @@
+// TODO: Wrap the entire contents of this file in an IIFE.
+// Pass in to the IIFE a module, upon which objects can be attached for later access.
 function Article (opts) {
   this.author = opts.author;
   this.authorUrl = opts.authorUrl;
@@ -7,11 +9,6 @@ function Article (opts) {
   this.publishedOn = opts.publishedOn;
 }
 
-// DONE: Instead of a global `articles = []` array, let's track this list of all articles directly on
-// Article <-- the **constructor function**. Note: ".all" is NOT on the prototype. In JS, functions are
-// objects; you can add properties to any function, at any time. In this app, we have an array of Article
-// objects that we want to build; that array does not belong in Article.prototype since that array is
-// "larger" than any single Article.
 Article.all = [];
 
 Article.prototype.toHtml = function() {
@@ -24,50 +21,71 @@ Article.prototype.toHtml = function() {
   return template(this);
 };
 
-// DONE: There are some other functions that also relate to articles across the board, rather than
-// just single instances. Object-oriented programming would call these "class-level" functions,
-// that are relevant to the entire "class" of objects that are Articles.
-
-// DONE: This function will take the rawData, how ever it is provided,
-// and use it to instantiate all the articles. This code is moved from elsewhere, and
-// encapsulated in a simply-named function for clarity.
 Article.loadAll = function(rawData) {
   rawData.sort(function(a,b) {
     return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
   });
 
-  rawData.forEach(function(ele) {
-    Article.all.push(new Article(ele));
+  // DONE: Refactor this forEach code, by using a `.map` call instead, since what we are
+  // trying to accomplish is the transformation of one colleciton into another.
+
+  // rawData.forEach(function(ele) {
+  //   Article.all.push(new Article(ele));
+  // })
+  Article.all = rawData.map(function(ele) {
+    return new Article(ele);
   });
 };
 
-//declaring variable outside of fetchAll to avoid scope issues
-
-
 // This function will retrieve the data from either a local or remote source,
 // and process it, then hand off control to the View.
+
+// TODO: Refactor this function, and provide it with a parameter of a callback function
+//(for now just a placeholder, but to be referenced at call time as a view function)
+// to execute once the loading of articles is done. We do this because we might want
+// to call other view functions, and not just the initIndexPage() that we are replacing.
+// Now, instead of calling articleView.initIndexPage(), we can simply run our callback.
 Article.fetchAll = function() {
   if (localStorage.rawData) {
-    // When rawData is already in localStorage,
-    // we can load it by calling the .loadAll function,
-    // and then render the index page (using the proper method on the articleView object).
-    Article.loadAll(
-      JSON.parse(localStorage.rawData)
-        //get local storage and parse it
-      //DONE: What do we pass in here to the .loadAll function?
-    );
-    articleView.initIndexPage(); //DONE: Change this fake method call to the correct one that will render the index page.
+    Article.loadAll(JSON.parse(localStorage.rawData));
+    articleView.initIndexPage();
   } else {
-    // 1. Retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
-    var getJasonData = $.getJSON('../data/ipsumArticles.json');
-
-    getJasonData.done(function (data) {
-      // 2. Store the resulting JSON data with the .loadAll method,
-      Article.loadAll(data);
-      // 3. Cache the data in localStorage so next time we won't enter this "else" block (avoids hitting the server),
-      localStorage.rawData = JSON.stringify(data);
-      // 4. Render the index page (perhaps with an articleView method?).
+    $.getJSON('/data/hackerIpsum.json', function(rawData) {
+      Article.loadAll(rawData);
+      localStorage.rawData = JSON.stringify(rawData); // Cache the json, so we don't need to request it next time.
       articleView.initIndexPage();
-    });    
+    });
   }
+};
+
+// TODO: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
+Article.numWordsAll = function() {
+  return Article.all.map(function(article) {
+    return whatShouldIReturn; // Grab the words from the `article` `body`.
+  })
+  .reduce(function(a, b) {
+    return whatShouldIReturn;// Sum up all the values!
+  });
+};
+
+// TODO: Chain together a `map` and a `reduce` call to produce an array of unique author names.
+Article.allAuthors = function() {
+  // Read docs on .map and .reduce! You can reference the previous
+  // `map` in the numWordsAll method to get started here.
+
+  // For our `reduce` -- since we are trying to return an array, we'll need to specify an accumulator type...
+  // what data type should this accumulator be and where is it placed?
+  return whatShouldIReturn;
+};
+
+Article.numWordsByAuthor = function() {
+  // TODO: Transform each author string into an object with 2 properties: One for
+  // the author's name, and one for the total number of words across the matching articles
+  // written by the specified author.
+  return Article.allAuthors().map(function(author) {
+    return {
+      // name:
+      // numWords: someCollection.someArrayMethod().map(...).reduce(...), ...
+    };
+  });
 };
