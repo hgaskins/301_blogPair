9c9d56c4202a455c89fdcda015a6c61ea5fba2d9
Hannah Gaskins <gaskinshannah@gmail.com>
2016-04-25 13:18:59 -0700

    articleController.js TODOs

--- scripts/articleController.js
+++ scripts/articleController.js
@@ -1,18 +1,17 @@
 (function(module) {
   var articlesController = {};
-
-
+  // DONE: Create the `articles` table when the controller first loads, with
+  //       code that was previously in index.html.
+  Article.createTable();
   articlesController.index = function() {
-    // TODO: Create the `articles` table when the controller first loads, with
-    //       code that was previously in index.html.
-    // TODO: Make the function below fetch and render the articles. It should look
+    // DONE: Make the function below fetch and render the articles. It should look
     //       very similiar to code you wrote before.
-    // TODO: Hide the main section elements; reveal the #articles section:
+    Article.fetchAll(articleView.initIndexPage);
+    // DONE: Hide the main section elements; reveal the #articles section:
+    $('main > section').hide();
+    $('#about').show();
 
   };
 
   module.articlesController = articlesController;
 })(window);
-
-Article.createTable();
-Article.fetchAll(articleView.initIndexPage);
