343959710758faed56e1332500fbe0d5b4c92c92
Hannah Gaskins <gaskinshannah@gmail.com>
2016-04-28 14:45:21 -0700

    articleController.js comments

--- scripts/articleController.js
+++ scripts/articleController.js
@@ -17,13 +17,24 @@
     Article.findWhere('id', ctx.params.id, articleData);
   };
 
-  // COMMENT: What does this method do?  What is it's execution path?
+
   articlesController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
       next();
     };
+    //SCAA n detail, explain how .findWhere() works.
+    /*
+    Article.findWhere() passes a string, a search parameter and a function to webdb.execute which then exectutes the SQL query
+    and calls the function on the results.
 
+    The authorName part is whatever string follows /author/ within the URL. page.js passes this in the ctx object when calling an
+    articlesController function.
+
+    page() on the routes.js script starts an event handler which watches the URL for changes, matches them to previously specified strings, and does the appropriately defined function calls.
+
+    ctx is the object passed to functions called from URL matches implemented by page.js.
+    */
     Article.findWhere('author', ctx.params.authorName.replace('+', ' '), authorData);
   };
 
